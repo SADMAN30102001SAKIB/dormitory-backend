03463dd9da608cfb653050fdde92834c61bb5bfc
heytamjid <tamjidhf@gmail.com>
2025-06-30 02:40:03 +0600

    fixed profile picture support

--- users/fields.py
+++ users/fields.py
@@ -6,24 +6,24 @@ from rest_framework import serializers
 
 class Base64ImageField(serializers.ImageField):
     """
-    A custom serializer field to handle base64-encoded images.
+    A Django REST Framework field for handling image-uploads, encoded in base64.
     """
 
     def to_internal_value(self, data):
-        """
-        Decode the base64 string into a file object.
-        """
-        # Check if the input is a base64 string
+        # Check if the base64 string is empty
+        if data == "":
+            return None
+
+        # Check if this is a base64 string
         if isinstance(data, str) and data.startswith("data:image"):
-            # 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQE...'
-            # Get the format and the base64 content
-            format, imgstr = data.split(";base64,")
-            ext = format.split("/")[-1]
-
-            # Generate a unique filename
-            filename = f"{uuid.uuid4()}.{ext}"
-
-            # Decode the base64 string
-            data = ContentFile(base64.b64decode(imgstr), name=filename)
-
+            # base64 encoded image - decode
+            try:
+                format, imgstr = data.split(";base64,")  # format ~= data:image/X,
+                ext = format.split("/")[-1]  # guess file extension
+                id = uuid.uuid4()
+                data = ContentFile(
+                    base64.b64decode(imgstr), name=id.urn[9:] + "." + ext
+                )
+            except Exception:
+                raise serializers.ValidationError("Invalid image.")
         return super().to_internal_value(data)
